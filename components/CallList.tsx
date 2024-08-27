28e89f227160d641e1c5117d88285245d357f636
Abhishek <abhishek.2101150ec@iiitbh.ac.in>
2024-08-27 23:51:54 +0530

    error fix

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,72 +1,67 @@
 'use client';
-import { useGetCalls } from '@/hooks/useGetCalls';
+
 import { Call, CallRecording } from '@stream-io/video-react-sdk';
-import { useRouter } from 'next/navigation';
-import React, { useEffect } from 'react'
-import MeetingCard from './MeetingCard';
+
 import Loader from './Loader';
-import { useToast } from './ui/use-toast';
+import { useGetCalls } from '@/hooks/useGetCalls';
+import MeetingCard from './MeetingCard';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
+const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
+  const router = useRouter();
+  const { endedCalls, upcomingCalls, callRecordings, isLoading } =
+    useGetCalls();
+  const [recordings, setRecordings] = useState<CallRecording[]>([]);
 
-function CallList({type}: {type: 'ended' | 'upcoming' | 'recordings'}) {
-    const { endedCalls, upcomingCalls, callRecordings, isLoading } = useGetCalls();
-    const router = useRouter();
-    const [recordings, setRecordings] = React.useState<CallRecording[]>([]);
-    const { toast } = useToast();
-
-    const getCalls = () => {  
-        switch (type) {
-            case 'ended':
-                return endedCalls;
-            case 'upcoming':
-                return upcomingCalls;
-            case 'recordings':
-                return callRecordings;
-            default:
-                return [];
-        }
+  const getCalls = () => {
+    switch (type) {
+      case 'ended':
+        return endedCalls;
+      case 'recordings':
+        return recordings;
+      case 'upcoming':
+        return upcomingCalls;
+      default:
+        return [];
     }
+  };
 
-    const getNoCallsMessage = () => {  
-        switch (type) {
-            case 'ended':
-                return 'No Previous Calls';
-            case 'upcoming':
-                return 'No Upcoming Calls';
-            case 'recordings':
-                return 'No Recordings';
-            default:
-                return '';
-        }
+  const getNoCallsMessage = () => {
+    switch (type) {
+      case 'ended':
+        return 'No Previous Calls';
+      case 'upcoming':
+        return 'No Upcoming Calls';
+      case 'recordings':
+        return 'No Recordings';
+      default:
+        return '';
+    }
+  };
+
+  useEffect(() => {
+    const fetchRecordings = async () => {
+      const callData = await Promise.all(
+        callRecordings?.map((meeting) => meeting.queryRecordings()) ?? [],
+      );
+
+      const recordings = callData
+        .filter((call) => call.recordings.length > 0)
+        .flatMap((call) => call.recordings);
+
+      setRecordings(recordings);
     };
 
-    useEffect(() => {
-        const fetchRecordings = async () => {
-            try {
-              const callData = await Promise.all(
-                callRecordings?.map((meeting) => meeting.queryRecordings()) ?? [],
-              );
-        
-              const recordings = callData
-                .filter((call) => call.recordings.length > 0)
-                .flatMap((call) => call.recordings);
-        
-              setRecordings(recordings);
+    if (type === 'recordings') {
+      fetchRecordings();
+    }
+  }, [type, callRecordings]);
 
-          } catch (error) {
-            toast({title : 'Try again later'})
-          }  
-        };
-    
-        if (type === 'recordings') {
-          fetchRecordings();
-        }
-      }, [type, callRecordings]);
-    
-      if (isLoading) return <Loader />;
+  if (isLoading) return <Loader />;
 
-    const calls = getCalls();
-    const noCallsMessage = getNoCallsMessage();
+  const calls = getCalls();
+  const noCallsMessage = getNoCallsMessage();
 
   return (
     <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
@@ -83,9 +78,8 @@ function CallList({type}: {type: 'ended' | 'upcoming' | 'recordings'}) {
             }
             title={
               (meeting as Call).state?.custom?.description ||
-              ((meeting as CallRecording).filename?.substring(0, 206) ||
-               (meeting as CallRecording).filename?.substring(0, 20) ||
-                'Personal Meeting Room')
+              (meeting as CallRecording).filename?.substring(0, 20) ||
+              'No Description'
             }
             date={
               (meeting as Call).state?.startsAt?.toLocaleString() ||
